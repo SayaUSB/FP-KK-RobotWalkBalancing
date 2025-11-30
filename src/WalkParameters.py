6a405d933f053c3c842f15abfcedb73158929d7f
SayaUSB <binasyahdiba@gmail.com>
2025-12-01 01:28:53 +0700

    refactor: Change parameters to default

--- src/WalkParameters.py
+++ src/WalkParameters.py
@@ -35,31 +35,31 @@ class WalkParameters:
     def _set_default_parameters(self):
         """Set default parameters"""
         # Timing parameters
-        self.params.single_support_duration = 0.6
+        self.params.single_support_duration = 0.38
         self.params.single_support_timesteps = 10
-        self.params.double_support_ratio = 0.0006008777616272946
-        self.params.startend_double_support_ratio = 1.5886696014263284
+        self.params.double_support_ratio = 0.0
+        self.params.startend_double_support_ratio = 1.5
         self.params.planned_timesteps = 48
         
         # Posture parameters
-        self.params.walk_com_height = 0.32236855497734307
-        self.params.walk_foot_height = 0.0373010486746116
-        self.params.walk_trunk_pitch = 0.1515944847805595
-        self.params.walk_foot_rise_ratio = 0.20113722567696113
+        self.params.walk_com_height = 0.32
+        self.params.walk_foot_height = 0.04
+        self.params.walk_trunk_pitch = 0.15
+        self.params.walk_foot_rise_ratio = 0.2
         
         # Feet parameters
         self.params.foot_length = 0.1576
         self.params.foot_width = 0.092
-        self.params.feet_spacing = 0.12185145577350875
-        self.params.zmp_margin = 0.01998778155747486
-        self.params.foot_zmp_target_x = -0.001560746760865157
-        self.params.foot_zmp_target_y = -0.00033707127920877284
+        self.params.feet_spacing = 0.122
+        self.params.zmp_margin = 0.02
+        self.params.foot_zmp_target_x = 0.0
+        self.params.foot_zmp_target_y = 0.0
         
         # Limit parameters
-        self.params.walk_max_dtheta = 1.0096951500288134
-        self.params.walk_max_dy = 0.04232503853606945
-        self.params.walk_max_dx_forward = 1.0044747278638186
-        self.params.walk_max_dx_backward = 0.02960790599310682
+        self.params.walk_max_dtheta = 1
+        self.params.walk_max_dy = 0.04
+        self.params.walk_max_dx_forward = 1
+        self.params.walk_max_dx_backward = 0.03
         
     def _define_tunable_bounds(self):
         """Define parameter bounds untuk optimization"""
