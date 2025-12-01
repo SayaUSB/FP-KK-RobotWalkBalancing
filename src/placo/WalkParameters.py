45e72919ed9268d129c1f1ce9489a973f4e36d95
SayaUSB <binasyahdiba@gmail.com>
2025-12-01 16:29:35 +0700

    feat:  Add json config support

--- src/placo/WalkParameters.py
+++ src/placo/WalkParameters.py
@@ -27,39 +27,70 @@ class ParameterBounds:
 class WalkParameters:
     """Parameters for robot gait with tuning support"""
     
-    def __init__(self):
+    def __init__(self, parameters: Dict=None):
         self.params = placo.HumanoidParameters()
+        self.parameters = parameters
         self._set_default_parameters()
         self._define_tunable_bounds()
         
     def _set_default_parameters(self):
         """Set default parameters"""
-        # Timing parameters
-        self.params.single_support_duration = 0.38
-        self.params.single_support_timesteps = 10
-        self.params.double_support_ratio = 0.0
-        self.params.startend_double_support_ratio = 1.5
-        self.params.planned_timesteps = 48
         
-        # Posture parameters
-        self.params.walk_com_height = 0.32
-        self.params.walk_foot_height = 0.04
-        self.params.walk_trunk_pitch = 0.15
-        self.params.walk_foot_rise_ratio = 0.2
-        
-        # Feet parameters
-        self.params.foot_length = 0.1576
-        self.params.foot_width = 0.092
-        self.params.feet_spacing = 0.122
-        self.params.zmp_margin = 0.02
-        self.params.foot_zmp_target_x = 0.0
-        self.params.foot_zmp_target_y = 0.0
-        
-        # Limit parameters
-        self.params.walk_max_dtheta = 1
-        self.params.walk_max_dy = 0.04
-        self.params.walk_max_dx_forward = 1
-        self.params.walk_max_dx_backward = 0.03
+        if not self.parameters:
+            # Timing parameters
+            self.params.single_support_duration = 0.38
+            self.params.single_support_timesteps = 10
+            self.params.double_support_ratio = 0.0 
+            self.params.startend_double_support_ratio = 1.5 
+            self.params.planned_timesteps = 48
+
+            # Posture parameters
+            self.params.walk_com_height = 0.32
+            self.params.walk_foot_height = 0.04
+            self.params.walk_trunk_pitch = 0.15
+            self.params.walk_foot_rise_ratio = 0.2
+
+            # Feet parameters
+            self.params.foot_length = 0.1576
+            self.params.foot_width = 0.092
+            self.params.feet_spacing = 0.122
+            self.params.zmp_margin = 0.02
+            self.params.foot_zmp_target_x = 0.0
+            self.params.foot_zmp_target_y = 0.0
+
+            # Limit parameters
+            self.params.walk_max_dtheta = 1
+            self.params.walk_max_dy = 0.04
+            self.params.walk_max_dx_forward = 1
+            self.params.walk_max_dx_backward = 0.03
+        else:
+            # Timing parameters
+            self.params.single_support_duration = self.parameters['single_support_duration']
+            self.params.single_support_timesteps = self.parameters['single_support_timesteps']
+            self.params.double_support_ratio = self.parameters['double_support_ratio']
+            self.params.startend_double_support_ratio = self.parameters['startend_double_support_ratio']
+            self.params.planned_timesteps = self.parameters['planned_timesteps']
+
+            # Posture parameters
+            self.params.walk_com_height = self.parameters['walk_com_height']
+            self.params.walk_foot_height = self.parameters['walk_foot_height']
+            self.params.walk_trunk_pitch = self.parameters['walk_trunk_pitch']
+            self.params.walk_foot_rise_ratio = self.parameters['walk_foot_rise_ratio']
+
+            # Feet parameters
+            self.params.foot_length = self.parameters['foot_length']
+            self.params.foot_width = self.parameters['foot_width']
+            self.params.feet_spacing = self.parameters['feet_spacing']
+            self.params.walk_max_dtheta = self.parameters['walk_max_dtheta']
+            self.params.walk_max_dy = self.parameters['feet_spacing']
+            self.params.foot_zmp_target_x = self.parameters['foot_zmp_target_x']
+            self.params.foot_zmp_target_y = self.parameters['foot_zmp_target_y']
+            
+            # Limit parameters
+            self.params.walk_max_dtheta = self.parameters['walk_max_dtheta']
+            self.params.walk_max_dy = self.parameters['walk_max_dy']
+            self.params.walk_max_dx_forward = self.parameters['walk_max_dx_forward']
+            self.params.walk_max_dx_backward = self.parameters['walk_max_dx_backward']
         
     def _define_tunable_bounds(self):
         """Define parameter bounds untuk optimization"""
